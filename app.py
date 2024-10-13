955c87f0e5dc633505e5703e54a15c31f19d23a3
isnaini <84210302+isnaini-ina@users.noreply.github.com>
2024-10-13 13:47:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_sc
 from sklearn.preprocessing import MinMaxScaler
 from imblearn.over_sampling import RandomOverSampler
 
-st.set_page_config(page_title="SKRIPSI-ina", page_icon='utm.png')
+st.set_page_config(page_title="SKRIPSI-ina", page_icon='logo-utm.png')
 @st.cache_data()
 def progress():
     with st.spinner('Wait for it...'):
@@ -24,8 +24,35 @@ def progress():
 with st.sidebar:
     selected = option_menu('',['Home', 'Research', 'Dataset', 'Preprocessing', 'Modelling', 'Implementation'], default_index=0)
 if (selected == 'Home'):
-    st.markdown("<h1 style='text-align: center; '>Klasifikasi Terhadap Resiko Penyakit Jantung dengan Menggunakan Metode Entropy Fuzzy Support Vector Machine</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; '>Klasifikasi terhadap Resiko Penyakit Jantung dengan Menggunakan Metode Entropy Fuzzy Support Vector Machine</h1>", unsafe_allow_html=True)
     url_logo = 'logo-utm.png'
+        col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write(' ')
+    with col2:
+        st.image(url_logo, use_column_width='auto')
+    with col3:
+        st.write(' ')
+     st.markdown("<h5 style='text-align: center; '>Diajukan Untuk Memenuhi Persyaratan Penyelesaian Studi Strata Satu (S1) dan Memperoleh Gelar Sarjana Komputer (S.Kom) di Universitas Trunojoyo Madura</h5>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; '>Disusun Oleh:</h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; '>Isnaini</h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; '>NPM:200411100038</h6>", unsafe_allow_html=True)
+
+    dp = [
+        ("Dosen Pembimbing 1", "Dr. Bain Khusnul Khotimah, S.T., M.Kom"),
+        ("Dosen Pembimbing 2", "Dr. Eka Mala Sari Rochman, S.Kom. M.Kom")
+    ]
+    dp_table = pd.DataFrame(dp, columns=["Role", "Name"])
+    st.table(dp_table)
+
+    dpn = [
+        ("Dosen Penguji 1", "Dwi Kuswanto, S.Pd., M.T."),
+        ("Dosen Penguji 2", "Hermawan, ST., M.Kom."),
+        ("Dosen Penguji 3", "Prof. Aeri Rachmad, S.T., M.T.")
+    ]
+    dpn_table = pd.DataFrame(dpn, columns=["Role", "Name"])
+    st.table(dpn_table)
+
 if (selected == 'Dataset'):
     st.title("Dataset")
     dataset, ket = st.tabs(['Dataset', 'Ket Dataset'])
