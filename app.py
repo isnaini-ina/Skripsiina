cf4b800317864f157282bc394f2ed2552310e746
isnaini <84210302+isnaini-ina@users.noreply.github.com>
2024-10-13 11:42:35 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
+
+from streamlit_option_menu import option_menu
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, confusion_matrix
+
+# prepocessing
+from sklearn.preprocessing import MinMaxScaler
+from imblearn.over_sampling import RandomOverSampler
+
+st.set_page_config(page_title="SKRIPSI-ina", page_icon='utm.png')
+@st.cache_data()
+def progress():
+    with st.spinner('Wait for it...'):
+        time.sleep(5)
+
+
+
+with st.sidebar:
+    selected = option_menu('',['Home', 'Research', 'Dataset', 'Preprocessing', 'Modelling', 'Implementation'], default_index=0)
+if (selected == 'Home'):
+    st.markdown("<h1 style='text-align: center; '>Implementasi Metode Smote Pada Klasifikasi Penyakit Stroke dengan Algoritma Naive Bayes</h1>", unsafe_allow_html=True)
+    url_logo = 'utm.png'
